3210a13357aae66b06428d6dcde3da142f4c76b4
Matheus Martins <124638744+MatheusMartinsR@users.noreply.github.com>
2023-08-23 20:40:39 -0300

    Create Desafio3.kt

---
+++ Desafio3.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    var nome: String = "Matheus Martins"
+    var idade: Int = 19
+    var email: String = "martinsmatheus2003@gmail.com"
+    var status: Boolean = true
+
+    println("O nome do usuário é: $nome")
+    println("A idade é: $idade")
+    println("O email: $email")
+    println("E o status é: $status")
+}
